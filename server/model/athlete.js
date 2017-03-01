b4b3f068df1de8327430be182b4ba70660f4d881
Andrew <andrew.thielcole@gmail.com>
2017-02-28 17:36:13 -0800

    Second Commit

---
+++ server/model/athlete.js
@@ -0,0 +1,26 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var athleteSchema = new Schema({
+  name: String,
+  overallrank: String,
+  overallscore: String,
+  regionid: String,
+  region: String,
+  affiliateid: String,
+  affiliate: String,
+  age: Number,
+  height: String,
+  weight: String,
+  division: Number,
+  scores: Array
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var athlete = mongoose.model('athletes', athleteSchema);
+
+// make this available to our users in our Node applications
+module.exports = athlete;
---
+++ server/routes/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Athlete = require('../model/athlete');
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+    // Add query methods here
+    let params = req.query.limit;
+    let query = Athlete.find({});
+    
+    if (params) {
+        query.limit(parseInt(params, 10));
+    }
+
+    query.exec((err, users) => {
+        if (err) throw err;
+
+        console.log(users);
+        res.status(200).json({
+            message: users
+        });
+    });
+});
+
+module.exports = router;
